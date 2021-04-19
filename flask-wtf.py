ed4f6d479c11619d9d10db0f4b257e46b5669b7a
INHIZARD <75397252+INHIZARD@users.noreply.github.com>
2021-04-19 21:22:43 +0300

    Update flask-wtf.py

--- flask-wtf.py
+++ flask-wtf.py
@@ -15,5 +15,10 @@ def training(prof):
     return render_template('base.html', prof=prof, n=1)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('base.html', list=list, n=2)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
